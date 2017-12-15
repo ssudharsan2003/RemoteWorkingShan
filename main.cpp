c4ea0c1b1b709002cfe4772b6f670287a3c49c5b
ssudharsan2003 <34582607+ssudharsan2003@users.noreply.github.com>
2017-12-15 14:11:12 -0500

    Change Last name of P2

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 
     Person p1("Joe", "Bloggs");
     cout << "\t" << p1.ToString() << endl;
-Person p2("John", "Mac");
+Person p2("John", "Loon");
     cout << "\t" << p2.ToString() << endl;
 
     cout << endl;
